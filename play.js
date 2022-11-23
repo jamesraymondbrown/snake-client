09a9c387a2ea6717d8b6df7fff7a3df7baf50bf5
James Brown <jamesraymondbrown@gmail.com>
2022-11-23 21:16:10 +0000

    moved setupInput to its own file, updated play.js to call that function

--- play.js
+++ play.js
@@ -1,22 +1,23 @@
 const net = require("net");
-const connect = require("./client")
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 // setup interface to handle user input from stdin
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
+// const handleUserInput = function (key) {
+//   if (key === '\u0003') {
+//     process.exit();
+//   }
+// }
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   stdin.on("data", handleUserInput);
+//   return stdin;
+// };
 
 console.log("Connecting ...");
 connect();
